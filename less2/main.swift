0a4f8325ca537fdef7e0383860b1d6ef72269257
bektur <bektur060525@gmail.com>
2022-11-22 19:43:14 +0600

    update

--- less2/main.swift
+++ less2/main.swift
@@ -1,11 +1,43 @@
-//
-//  main.swift
-//  less2
-//
-//  Created by Бектур Каримов on 18/11/22.
-//
+//import Foundation
 
-import Foundation
+//var numberToShow = 9
+//if numberToShow == 10 {
+//    print("число меньше или равно 10")
+//}else if numberToShow > 10{
+//    print("число больше 10")
+//}else {
+//    print("число не равно 10 и не больше 10")
+//}
 
-print("Hello, World!")
+
+// >= - болтше или равно
+//&& - и
+//== - =
+//!= - не равно
+//|| - or
+
+//print(numberToShow >= 10)
+//
+//func printAbdult (num: Int){
+//    if num >= 18{
+//        print("yes")
+//    }else{
+//        print("no")
+//    }
+//}
+
+//printAbdult(num: 10)
+//printAbdult(num: 23)
+
+ 
+
+func num1 ( num: Int){
+    if num == 50{
+        print("yeaaaa")
+    }else{
+        print ("wtf")
+    }
+    }
+
+num1(num: 45)
 
